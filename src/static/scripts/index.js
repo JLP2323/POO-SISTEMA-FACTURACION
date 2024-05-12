e0521af037ec84e17aca280b6730992feae586fe
Ing-Javier-Sistem <145406699+Ing-Javier-Sistem@users.noreply.github.com>
2024-05-12 16:40:08 -0500

    update global R3 a main - R1 - R2

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,30 +1,63 @@
 import { MethodsModular } from "src/classes/classModularMethods.js";
 
-// Crear una instancia de MethodsModular
 const methodsModularInstance = new MethodsModular();
 
 document.addEventListener('DOMContentLoaded', function () {
-    const formClient = document.getElementById("formClient");
 
-    formClient.addEventListener('submit', function (event) {
-        event.preventDefault();
-        methodsModularInstance.resetForm(formClient);
-        // formClient.submit();
+    // Manejo de las SECTION del DOM --- no tocar
+    const lastSectionId = localStorage.getItem('lastSectionId');
+    showSection(lastSectionId || 'containerClientes');
+
+    function showSection(sectionId) {
+        document.querySelectorAll('main section').forEach(section => {
+            section.style.display = 'none';
+        });
+        document.getElementById(sectionId).style.display = 'block';
+        localStorage.setItem('lastSectionId', sectionId);
+    }
+
+    document.querySelectorAll('nav button').forEach(button => {
+        button.addEventListener('click', function () {
+            const sectionId = this.getAttribute('data-section-id');
+            showSection(sectionId);
+        });
     });
 
-    const formProduct = document.getElementById("formProduct");
-
-    formProduct.addEventListener('submit', function (event) {
+    
+    const btnSaveClient = document.getElementById("btnSaveClient");
+    btnSaveClient.addEventListener('click', (event) => {
         event.preventDefault();
-        methodsModularInstance.resetForm(formProduct);
-        // formClient.submit();
+        dataCliente();
+
+        Limpiar_Inputs();
     });
 
-    const formInvoice = document.getElementById("formInvoice");
+    function dataCliente() {
+        const dni = document.getElementById("dni").value;
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        
+        if (dni && firstName && lastName) {
+            let dataCliente = JSON.parse(localStorage.getItem('KEY_dataCliente')) || [];
+            dataCliente.push({
+                KEY_dni: dni,
+                KEY_firstName: firstName,
+                KEY_lastName: lastName,
+                KEY_descont: 0.10,
+            });
+            localStorage.setItem('KEY_dataCliente', JSON.stringify(dataCliente));
+        }
+    }
 
-    formInvoice.addEventListener('submit', function (event) {
-        event.preventDefault();
-        methodsModularInstance.resetForm(formInvoice);
-        // formClient.submit();
-    });
+    function Limpiar_Inputs() {
+        const remove_inputs = [
+            "dni",
+            "firstName",
+            "lastName",
+        ];
+        remove_inputs.forEach((remove_inputs) => {
+            document.getElementById(remove_inputs).value = ""; 
+        });
+    }
 });
+
